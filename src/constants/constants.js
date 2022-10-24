b56bca16734b330845c2f49fa825a674ffd3e773
ali-elmowed <almowed.ali@gmail.com>
2022-10-24 15:58:59 +0300

    New

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 // const CLIENT_ID = "4a9363edd9b14913b113065505b9a107";
-const CLIENT_ID = "48fbc3e03f8b45dfa2c28a1f6756dbca";
+const CLIENT_ID = "f0fe1ceb1ec14e32b42a675dea9a30cf";
 const REDIRECT_URI = "http://localhost:3000";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
